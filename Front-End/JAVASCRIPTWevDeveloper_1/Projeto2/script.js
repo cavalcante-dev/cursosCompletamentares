a9a3175199b0636e0376e2c4c48ded564f45c496
cavalcante-dev <leandrocavalcante2504@gmail.com>
2025-07-10 13:49:17 -0300

    update javascript

--- Front-End/JAVASCRIPTWevDeveloper_1/Projeto2/script.js
+++ Front-End/JAVASCRIPTWevDeveloper_1/Projeto2/script.js
@@ -1,30 +1,86 @@
+let tarefas = [];    
+const mensagem = document.getElementById("mensagem");
 
 function adicionarTarefa() {
     
-    let mensagemPos = "Tarefa adicionada com sucesso!";
-    let mensagemNeg = "O campo está vazio!";
+    const addTarefa = document.getElementById("addTarefa");
+    let tarefa = addTarefa.value.trim();
 
-    let addTarefa = document.getElementById("addTarefa");
-    let tarefa = addTarefa.value;
-    
+    const mensagemPos = "Tarefa adicionada com sucesso!";
+    const mensagemNeg = "O campo está vazio!";
 
     if (!tarefa) {
-
-        document.getElementById("mensagem").textContent = mensagemNeg;
-        
+        mensagem.textContent = mensagemNeg;
     } else {
-
-        document.getElementById("mensagem").textContent = mensagemPos;
-
-        let listaTarefas = document.getElementById("listaTarefas");
-        let novaTarefa = document.createElement("li");
-
-        novaTarefa.textContent = tarefa;
-
-        listaTarefas.appendChild(novaTarefa);
-
+        mensagem.textContent = mensagemPos;
+        tarefas.push(tarefa);
+        renderizarTarefas();
     }
 
     addTarefa.value = "";
 
+}
+
+function renderizarTarefas() {
+
+    let listaTarefas = document.getElementById("listaTarefas");
+
+    listaTarefas.innerHTML = "";
+
+    if(tarefas.length > 0) {
+        
+        let botaoRemoverTudo = document.createElement("button");
+        botaoRemoverTudo.className = "removerTodos";
+        botaoRemoverTudo.textContent = "Limpar Lista"
+        botaoRemoverTudo.onclick = () => limparTarefas();
+        
+        let container = document.getElementById("botoes");
+        container.appendChild(botaoRemoverTudo);
+
+    } 
+
+    for(let i = 0; i < tarefas.length; i++) {
+
+        let novaTarefa = document.createElement("li");
+        novaTarefa.textContent = tarefas[i];
+        
+        let botaoRemover = document.createElement("button");
+        botaoRemover.className = "remover";
+        botaoRemover.textContent="X";
+        botaoRemover.onclick = () => removerTarefa(i);
+
+        let botaoEditar = document.createElement("button");
+        botaoEditar.className = "editar";
+        botaoEditar.textContent="Editar";
+        botaoEditar.onclick = () => editarTarefa(i);
+
+        novaTarefa.appendChild(botaoRemover);    
+        novaTarefa.appendChild(botaoEditar);
+        listaTarefas.appendChild(novaTarefa);
+
+    }
+
+}
+
+function removerTarefa(i) {
+    tarefas.splice(i, 1);
+    mensagem.textContent = "Tarefa excluida com sucesso!"
+    renderizarTarefas();
+}
+
+function editarTarefa(i) {
+    let tarefaEditada = prompt('Edite a sua tarefa: ');
+    if (tarefaEditada.trim() !== "") {
+        tarefas[i] = tarefaEditada;
+        mensagem.textContent = "Tarefa editada com sucesso!"
+        renderizarTarefas();
+    } else {
+        tarefaEditada = prompt('Tarefa em branco!');
+    }
+}
+
+function limparTarefas() {
+    tarefas.length = 0;
+    mensagem.textContent = "Lista limpa com sucesso!"
+    renderizarTarefas();
 }
